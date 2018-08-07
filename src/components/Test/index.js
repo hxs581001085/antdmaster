9aa9e026d8c87d4df4f2aff2fbd306d855a9ef76
hxs581001085 <hxs581001085@163.com>
2018-08-07 10:12:52 +0800

    初始化Git目录

---
+++ src/components/Test/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function Result() {
+  return (
+    <h1>hehe, welcome to home</h1>
+  );
+}
---
+++ src/routes/Dashboard/Home.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+import Test from '../../components/Test';
+
+
+export default class HomePage extends PureComponent {
+	render() {
+		return (
+			<Test />
+		)
+	}
+}
